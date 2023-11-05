724f40ad2dcc87ca5f2e545bbb6a77e3ee438147
Prabhjot Singh <Prabhx.jott@gmail.com>
2023-11-06 03:46:20 +0530

    Resolve naming error

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,13 @@ from api.views import BookViewSet, BookPartialUpdateView
 from rest_framework import routers
 
 # Create a default router for handling API endpoints.
-api_router = routers.DefaultRouter()
-api_router.register('books', BookViewSet)
+router = routers.DefaultRouter()
+router.register('books', BookViewSet)
 
 # Define URL patterns for the project.
 urlpatterns = [
     # Include API endpoint routes provided by the 'api_router'.
-    path('', include(api_router.urls)),
+    path('', include(router.urls)),
     
     # Define a custom URL pattern for partially updating a book using its primary key (pk).
     path('books/<int:pk>/partial-update/', BookPartialUpdateView.as_view(), name='book-partial-update'),
